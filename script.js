cbcef9741f619adee97f56e0ef23dfb80716ed36
ALLANALOE <104464433+ALLANALOE@users.noreply.github.com>
2024-10-29 07:40:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Display loading animation for 3 seconds, then redirect to the main page
+    setTimeout(function() {
+        window.location.href = "page.html"; // Redirect to the main page
+    }, 3000); // 3000 milliseconds = 3 seconds
+});
---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Show loading animation for 3 seconds
+    setTimeout(function() {
+        document.getElementById('cybex5-intro').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }, 3000); // 3000 milliseconds = 3 seconds
+});
